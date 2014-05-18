1576699a10f5a2a7e96ba7f9a4e5e2c7e6072612
Dylan Butman <dbutman@nyu.edu>
2014-05-17 22:19:10 -0400

    remove polynome

--- project.clj
+++ project.clj
@@ -8,5 +8,5 @@
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [overtone "0.9.1"]
-                 [polynome "0.3.0-SNAPSHOT"]])
+                 [overtone "0.9.1"]])
+x
