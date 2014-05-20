dc22ebdebd8d05184851561aba63845b2e224c67
Dylan Butman <dbutman@nyu.edu>
2014-05-20 10:22:20 -0400

    tap-events

--- src/monome_osc/events.clj
+++ src/monome_osc/events.clj
@@ -10,13 +10,21 @@
 
 (defn reset-events! [device]
   (let [id (get-in device [:info :id])
-        listener (pub (listen-to device) first)]
+        listener (listen-to device)]
     (when-let [old (get @events id)]
       (close! old))
-    (swap! events assoc id listener)))
+    (swap! events assoc id {:listener  listener
+                            :mult (mult listener)
+                            :pub  (pub listener first)})))
 
 (defn sub-events [device key]
   (let [{:keys [id]} (:info device)
         out (chan (sliding-buffer 1))]
-    (sub (get @events id) key out)
+    (sub (get-in @events [id :pub]) key out)
     (map< second out)))
+
+(defn tap-events [device]
+  (let [{:keys [id]} (:info device)
+        out (chan (sliding-buffer 1))]
+    (tap (get-in @events [id :mult]) out)
+    out))
